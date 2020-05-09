8007416d0c7db980d0e26c38cf1cfc9b0a1a42a2
Fahad Hussain <fahadhussain0127@gmail.com>
2020-05-09 07:35:34 +0500

    React Navigation 1
    Added React navigation in the project

--- components/DishDetail.js
+++ components/DishDetail.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { Card } from "react-native-elements";
+import { DISHES } from "../shared/dishes";
 
 function RenderDish(props) {
   const dish = props.dish;
@@ -8,9 +9,9 @@ function RenderDish(props) {
     return (
       <Card
         featuredTitle={dish.name}
-        //image={require("./images/uthappizza.jpg")}
+        image={require("./images/uthappizza.png")}
       >
-        <Text styele={{ margin: 10 }}>{dish.description}</Text>
+        <Text style={{ margin: 10 }}>{dish.description}</Text>
       </Card>
     );
   } else {
@@ -18,8 +19,21 @@ function RenderDish(props) {
   }
 }
 
-function DishDetail(props) {
-  return <RenderDish dish={props.dish}></RenderDish>;
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Dish Details",
+  };
+  render() {
+    const dishId = this.props.navigation.getParam("dishId", "");
+    return <RenderDish dish={this.state.dishes[+dishId]}></RenderDish>;
+  }
 }
 
 export default DishDetail;
--- components/maincomponent.js
+++ components/maincomponent.js
@@ -1,36 +1,38 @@
+import "react-native-gesture-handler";
 import React, { Component } from "react";
 import Menu from "./menucomponent";
 import { DISHES } from "../shared/dishes";
 import DishDetail from "./DishDetail";
-import { View } from "react-native";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+const navigator = createStackNavigator(
+  {
+    menu: Menu,
+    dishdetail: DishDetail,
+  },
+  {
+    intialRouteName: "menu",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MenuNavigator = createAppContainer(navigator);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null,
-    };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
     return (
-      <View>
-        <Menu
-          dishes={this.state.dishes}
-          onPress={(dishId) => this.onDishSelect(dishId)}
-        ></Menu>
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        ></DishDetail>
+      <View style={{ flex: 1, paddingTop: Expo.Constants.statusBarHeight }}>
+        <MenuNavigator></MenuNavigator>
       </View>
     );
   }
